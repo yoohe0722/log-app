c64e20298c91eea49cb7c84595dcb1d8b8b64cfe
Yohei Higashi <yoohe0722@gmail.com>
2019-09-03 03:01:34 +0900

    config_path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,21 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','javascripts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','stylesheets')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','fontawesome-free')
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/font-awesome-rails-4.7.0.5/app/assets/fonts",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/font-awesome-rails-4.7.0.5/app/assets/stylesheets",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/jquery-rails-4.3.5/vendor/assets/javascripts",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/coffee-rails-4.2.2/lib/assets/javascripts",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/actioncable-5.2.3/lib/assets/compiled",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/activestorage-5.2.3/app/assets/javascripts",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/actionview-5.2.3/lib/assets/compiled",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/turbolinks-source-5.2.0/lib/assets/javascripts",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/bootstrap-sass-3.3.7/assets/stylesheets",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/bootstrap-sass-3.3.7/assets/javascripts",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/bootstrap-sass-3.3.7/assets/fonts",
+#  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/bootstrap-sass-3.3.7/assets/images"]
