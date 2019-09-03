d28484d6fb8b64bf8783413845f5e049403ca052
Yohei Higashi <yoohe0722@gmail.com>
2019-09-03 18:53:04 +0900

    edit_path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,10 +14,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','images')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','javascripts')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','stylesheets')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','fontawesome-free')
+Rails.application.config.assets.paths << Rails.root.join('app','assets','images')
+Rails.application.config.assets.paths << Rails.root.join('app','assets','javascripts')
+Rails.application.config.assets.paths << Rails.root.join('app','assets','stylesheets')
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','fontawesome-free','css')
 #  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/font-awesome-rails-4.7.0.5/app/assets/fonts",
 #  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/font-awesome-rails-4.7.0.5/app/assets/stylesheets",
 #  "/Users/yoheihigashi/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/jquery-rails-4.3.5/vendor/assets/javascripts",
